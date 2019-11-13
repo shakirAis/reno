94416604db6ac7ba7888ecd82b48345500699a24
Aisylu <aisilunya@gmail.com>
2019-11-13 11:37:45 +0100

    Mobyle version almost is compleated

--- libs/php/form.php
+++ libs/php/form.php
@@ -1,16 +1,16 @@
 <?php
     $name = $_POST['name'];
     $phone = $_POST['phone'];
-    $date = $_POST['date'];
-    $object = $_POST['object'];
+    $choosenDate = $_POST['choosenDate'];
+    $choosenObject = $_POST['choosenObject'];
    
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= "Content-type: text/html; charset=utf-8 \r\n";
     $headers .= "From: noreply <Почтовый сервер Haus Master>\r\n";
     if(mail("aisilunya@gmail.com", "Заявка из сайта о вызове мастера", 
     "$name оставил заявку<br/>
-    на дату:$date</br>
-    по объекту: $object<br/>
+    на дату:$choosenDate</br>
+    по объекту: $choosenObject<br/>
     Номер телефона клиента: $phone<br/>
     Срочно ему перезвоните, пожалуйста!",
     $headers)) {
